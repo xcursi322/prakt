626beb209e0ea856f810914c96c13f7938821184
xcursi322 <gondalvin243@gmail.com>
2026-04-09 20:34:34 +0300

    файли для рендеру

--- shop/models.py
+++ shop/models.py
@@ -84,7 +84,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)  # Ціна
     old_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, help_text="Стара ціна")
     stock_quantity = models.PositiveIntegerField(default=0, help_text="Кількість в наявності (використовується тільки якщо немає смаків)")
-    description = models.TextField()              # Опис
+    description = models.TextField(blank=True)     # Опис
     image = models.ImageField(upload_to='products/', blank=True, null=True)  # Головне зображення
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')  # Категорія
     created_at = models.DateTimeField(auto_now_add=True)  # Дата створення
